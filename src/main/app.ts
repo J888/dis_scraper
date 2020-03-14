a9c7a434ed6a3026595654bc62b1d5c99b32d254
John H <0xjohnh@gmail.com>
2020-03-14 11:54:56 -0400

    Add ability to scrape on demand via an endpoint (#3)

--- src/main/app.ts
+++ src/main/app.ts
@@ -8,43 +8,54 @@ import { writeOutToFile } from './util/fileUtil'
 const PARK_HOURS_URL = process.env.PARK_HOURS_URL
 const PARK_HOURS_GENERATED_DATA_PATH = 'scraped_data/parkHours.json'
 const ENDPOINTS = {
-  PARK_TIMES: '/park-times'
+  PARK_TIMES: '/park-times',
+  RUN_SCRAPE: '/park-times/scrape'
 }
 
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 8080
 
-app.get(ENDPOINTS.PARK_TIMES, async (req, res) => {
-    fs.readFile(PARK_HOURS_GENERATED_DATA_PATH, {encoding: 'utf-8'}, (err, data) => {
-      if (err)
-        return res.send('There was an error getting the data').status(500)
+const runScrape = async () => {
+  let data: any
 
-      return res.json(JSON.parse(data)).status(200)
-    })
+  const htmlRes = await axios({
+    url: PARK_HOURS_URL,
+    method: 'GET',
+    responseType: 'blob'
+  })
+
+  const { data: html } = htmlRes
+
+  const wdwScraper = new WdwScraper(html)
+
+  data = await wdwScraper.scrapeParkTimes()
+  writeOutToFile(data, PARK_HOURS_GENERATED_DATA_PATH)
+}
+
+app.get(ENDPOINTS.PARK_TIMES, async (_req, res) => {
+  fs.readFile(PARK_HOURS_GENERATED_DATA_PATH, {encoding: 'utf-8'}, (err, data) => {
+    if (err)
+      return res.send('There was an error getting the data').status(500)
+
+    return res.json(JSON.parse(data)).status(200)
+  })
+})
+
+app.get(ENDPOINTS.RUN_SCRAPE, (_req, res) => {
+
+  runScrape()
+  return res.status(200).send('Ran scrape')
 })
 
 app.listen(port, async () => {
   console.log(`server started at http://localhost:${port}`)
-
-  let data: any
 
   /* Runs at 07:00 */
   const job = new CronJob('0 7 * * *', async () => {
 
     console.log('Running cron job to scrape park times from WDW site')
 
-    const htmlRes = await axios({
-      url: PARK_HOURS_URL,
-      method: 'GET',
-      responseType: 'blob'
-    })
-
-    const { data: html } = htmlRes
-
-    const wdwScraper = new WdwScraper(html)
-
-    data = await wdwScraper.scrapeParkTimes()
-    writeOutToFile(data, PARK_HOURS_GENERATED_DATA_PATH)
+    runScrape()
 
   }, null, true, 'America/New_York')
 
